c3e6d9792c1a400c45dbaf9a3875d3b4df613dfc
Natalia <43728977+shpinatashan@users.noreply.github.com>
2018-11-09 11:46:12 +0300

    Add files via upload

--- safe.cpp
+++ safe.cpp
@@ -22,7 +22,7 @@ Node* Create_Node (int value);
 Node* Insert_Node (Node* cur, int value);
 Node* Search_Node (Node* cur, int value);
 int   Indexx_Node (Node* cur, Node* list_ptr);
-void  Delete_Node (Node* cur);
+void  Delete_single_Node (Node* cur, Node* list_ptr);
 void  Swap___Node (Node* ptr1, Node* ptr2);
 int   OK_Node(Node* ptr);
 void  Calculate_CHCKSM(Node* ptr);
@@ -47,10 +47,11 @@ if (scanf("%d", &head) != 1)
     }
 
 Node* list_ptr = Create_Node (head);
-
-if (OK_Node(list_ptr) < 0)
-    printf ("ERROR list_ptr");
-
+if (list_ptr == NULL)
+    {
+        printf("NULL ptr");
+        return -2;
+    }
 Node* cur = list_ptr;
 for (int i = 1; i < number; i++)
 {
@@ -62,8 +63,11 @@ for (int i = 1; i < number; i++)
         return -1;
     }
     cur = Insert_Node(cur, num);
-    if (OK_Node(cur) < 0)
-        printf ("ERROR insert cur\n");
+    if (cur == NULL)
+    {
+        printf("NULL ptr");
+        return -2;
+    }
 }
 
 int value = 0;
@@ -75,33 +79,24 @@ if (scanf("%d", &value) != 1)
 }
 
 Node* searches = Search_Node (list_ptr, value);
-Calculate_CHCKSM(searches);
-if (OK_Node(searches) < 0)
-    printf ("ERROR search\n");
-
+    //находим индекс найденного узла
 cur = searches;
 int index = Indexx_Node (cur, list_ptr);
 if (index > 0)
     printf("index = %d\n", index-1);
 
+    //Ќайденный узел  ставим на место первого. ѕервый затем удал€етс€.
 Node* tmp = list_ptr;
 Swap___Node (cur, tmp);
-Calculate_CHCKSM(cur);
-Calculate_CHCKSM(tmp);
-if (OK_Node(cur) < 0)
-    printf ("ERROR cur \n");
-if (OK_Node(tmp) < 0)
-    printf ("ERROR tmp \n");
 
-Delete_Node (cur);
 if (cur == list_ptr)
     list_ptr = cur -> next;
+Delete_single_Node (cur, list_ptr);
 
-printf("list: ");
+
+printf("\nlist: ");
 Printf_Node (list_ptr);
 
-free (list_ptr);
-list_ptr = NULL;
 return 0;
 }
 
@@ -110,24 +105,24 @@ int OK_Node(Node* ptr)
     if (ptr == NULL)
     {
         printf("ptr \n");
-        return -1;
+        return -21;
     }
 
     if ( ptr -> checksum != ( ((unsigned long long int) (ptr -> next) + (unsigned long long int)(ptr -> prev ) )% 12978 ))
     {
         printf("CHKSUM\n");
-        return -2;
+        return -22;
     }
     if ( ptr -> can1 != can)
     {
         printf("can1\n");
-        return -3;
+        return -23;
     }
 
     if ( ptr -> can2 != can)
     {
         printf("can2\n");
-        return -4;
+        return -24;
     }
 
 return 1;
@@ -136,7 +131,11 @@ return 1;
 void Calculate_CHCKSM(Node* ptr)
 {
     if (ptr == NULL)
-        return;
+    {
+        printf("NULL ptr");
+       return;
+    }
+
     ptr -> checksum = ( ((unsigned long long int) (ptr -> next) + (unsigned long long int)(ptr -> prev ) )% 12978 );
 }
 
@@ -151,14 +150,18 @@ Node* Create_Node (int value)
     list_ptr -> can2 = can;
 
     Calculate_CHCKSM(list_ptr);
-    OK_Node(list_ptr);
+    if (OK_Node(list_ptr) < 0)
+    {
+      printf ("ERROR in Create_Node");
+      return NULL;
+    }
 
 return list_ptr;
 }
 
 Node* Insert_Node(Node* cur, int value)
 {
-    if (cur == NULL)
+    if (OK_Node(cur) < 0)
     {
         printf ("Error in Insert_Node");
         return NULL;
@@ -172,34 +175,52 @@ Node* Insert_Node(Node* cur, int value)
     new_n -> can1 = can;
     new_n -> can2 = can;
     Calculate_CHCKSM(new_n);
-    if (new_n -> next == NULL)
+    if (OK_Node(new_n) < 0)
     {
-        OK_Node(new_n);
-        return new_n;
+        printf ("ERROR in Insert_Node\n");
+        return NULL;
     }
+    if (new_n -> next == NULL)
+        return new_n;
 
     (new_n -> next) -> prev = new_n;
     new_n  -> can1 = can;
     new_n  -> can2 = can;
-    OK_Node(new_n);
+    if (OK_Node(new_n) < 0)
+    {
+        printf ("ERROR in Insert_Node\n");
+        return NULL;
+    }
 
 return new_n;
 }
 
 Node* Search_Node (Node* list_ptr, int value)
 {
-    if (list_ptr == NULL)
+    Calculate_CHCKSM(list_ptr);
+    if (OK_Node(list_ptr) < 0)
     {
-        printf ("Error in Search_Node");
+        printf ("ERROR in Search_Node\n");
         return NULL;
     }
+
     if (list_ptr -> data == value)
         return list_ptr;
+
+
     Node* cur = list_ptr;
 
     while(cur != NULL)
     {
-        if(cur -> data == value) return cur;
+        Calculate_CHCKSM(cur);
+        if (OK_Node(cur) < 0)
+        {
+           printf ("ERROR search\n");
+           return NULL;
+        }
+
+        if(cur -> data == value)
+            return cur;
         cur = (cur -> next);
     }
     printf("Sorry!There is no such element \n");
@@ -229,7 +250,8 @@ void Printf_Node (Node* list_ptr)
 {
     if (list_ptr == NULL)
     {
-        printf ("Error in Printf_Node");
+        printf ("Error in Printf_Node/The list is empty");
+        return;
     }
     Node* p = list_ptr;
 
@@ -246,40 +268,47 @@ void Swap___Node (Node* ptr1, Node* ptr2)
 {
     if ((ptr1 != NULL)&& (ptr2 != NULL))
     {
-
-        Node* tmp = (Node*) calloc (1, sizeof(Node));
-
-        (tmp  -> data) = (ptr1 -> data);
+        int tmp;
+        tmp = (ptr1 -> data);
         (ptr1 -> data) = (ptr2 -> data);
-        (ptr2 -> data) = (tmp  -> data);
+        (ptr2 -> data) = tmp;
+        Calculate_CHCKSM(ptr1);
+        Calculate_CHCKSM(ptr2);
+        if (OK_Node(ptr1) < 0)
+            printf ("ERROR ptr \n");
+        if (OK_Node(ptr2) < 0)
+            printf ("ERROR ptr \n");
 
     }
 }
 
-void Delete_Node(Node* cur)
+void Delete_single_Node(Node* cur, Node* list_ptr)
 {
-    if (cur == NULL)
+    if (OK_Node(cur) < 0)
     {
         printf ("Error in Delete_Node");
         return;
     }
+
     if (cur -> prev == NULL)
     {
         if (cur -> next == NULL)
-        return;
+        return ;
     }
+
     if (cur -> prev == NULL)
     {
-        (cur -> next) -> prev = (cur -> prev);
+        (cur -> next) -> prev = NULL;
         return;
     }
     if (cur -> next == NULL)
     {
-        (cur -> prev) -> next = (cur -> next);
+        (cur -> prev) -> next = NULL;
         return;
     }
     (cur -> prev) -> next = (cur -> next);
     (cur -> next) -> prev = (cur -> prev);
 
     free (cur);
+    cur = NULL;
 }
